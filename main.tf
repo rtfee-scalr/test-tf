558789aac7c1b86b8a1d2b6c06c402af1ffab0aa
rtfee-scalr <42618766+rtfee-scalr@users.noreply.github.com>
2019-08-21 14:46:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ resource "aws_instance" "my_app" {
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
   key_name               = "ryan"
+    tags = {
+    Name = "${var.tags}"
+  }
 }
 
 output "instance_public_ips" {
