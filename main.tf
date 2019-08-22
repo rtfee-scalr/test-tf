691c7e904bbf2c5da8ad5b78cebec8409f28077a
rtfee-scalr <42618766+rtfee-scalr@users.noreply.github.com>
2019-08-22 10:43:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_app" {
   count = 1
   ami                    = "ami-2757f631"
-  instance_type          = "${var.instance_type}"
+  instance_type          = "t2.nano"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
   key_name               = "ryan"
