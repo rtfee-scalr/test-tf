3649996bdacb029f4d8c65ed2f22077a45c1e95e
rtfee-scalr <42618766+rtfee-scalr@users.noreply.github.com>
2019-08-29 14:39:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 resource "aws_instance" "scalr" {
   count = 1
+  region = "${var.region}"
   ami                    = "ami-2757f631"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0ebb1058ad727cfdb"
   vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
   key_name               = "ryan"
 }
+
 output "instance_public_ips" {
   value = "${aws_instance.scalr.*.id}"
 }
