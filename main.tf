22009e93455ea29a2c560387bca2b3eafcb748da
rtfee-scalr <42618766+rtfee-scalr@users.noreply.github.com>
2021-03-11 14:15:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = var.scalr_aws_access_key
   secret_key = var.scalr_aws_secret_key
-  region     = var.region
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "scalr" {
